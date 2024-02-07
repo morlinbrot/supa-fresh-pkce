e47f3c1279e98289192c27cc74e61a4c50116093
morlinbrot <morlinbrot@mailbox.org>
2024-02-07 21:19:57 +0100

    Update sign-out route

--- routes/api/sign-out.ts
+++ routes/api/sign-out.ts
@@ -1,14 +1,18 @@
 import { Handlers } from "$fresh/server.ts";
-import { deleteCookie } from "$std/http/cookie.ts";
+import { createSupabaseClient } from "lib/supabase.ts";
 
 export const handler: Handlers = {
-  GET(req) {
-    const url = new URL(req.url);
-    const headers = new Headers(req.headers);
-
-    deleteCookie(headers, "auth", { path: "/", domain: url.hostname });
-
+  async GET(req) {
+    const headers = new Headers();
     headers.set("location", "/");
+
+    const supabase = createSupabaseClient(req, headers);
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error(error);
+      return new Response(null, { status: 500, headers });
+    }
 
     return new Response(null, {
       status: 302,
